8461d850d752022b26cfd28ec56ab38314a6e755
Lclocker <47572247+Lclocker@users.noreply.github.com>
2021-09-23 18:21:47 -0300

    estrutura do projeto e splashScreen

---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:pharmacy_wiki/main.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+///Retorna a SplashScreen do app
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    // ignore: await_only_futures
+    return await Timer(_duration, navigationPage);
+  }
+
+  Future<void> navigationPage() async {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) =>
+                MyHomePage(title: "Pharmacy Wiki")));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.lightBlue, Colors.white])),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+              child: SizedBox(
+                  width: 150.0,
+                  child: Image.asset(
+                    "imagens/lindo.jpeg",
+                    fit: BoxFit.contain,
+                  )))),
+    );
+  }
+}
